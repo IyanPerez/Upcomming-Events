f9baf658e7caa237a1c39d48ad89e64363045de2
Caren Salles <carencrisbsalles@gmail.com>
2023-02-14 14:55:17 +0100

    basic structor

---
+++ src/main/java/com/sala78/upcommingevents/UpcommingEventsApplication.java
@@ -0,0 +1,13 @@
+package com.sala78.upcommingevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcommingEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcommingEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sala78/upcommingevents/UpcommingEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sala78.upcommingevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcommingEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
