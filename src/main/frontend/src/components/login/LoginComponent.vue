823276002b082dd8615212aafcc4261c02d272a6
Caren Salles <carencrisbsalles@gmail.com>
2023-02-17 15:34:48 +0100

    LoginCompoent has been created

---
+++ src/main/frontend/src/components/login/LoginComponent.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+</script>
+
+<template>
+
+<main>   
+
+    <div class="login-container">
+        <img class="login-container__img" src="../../assets/img/box.png" alt="icon-person">
+            <div class="login-container__title">
+                <h1>Login to account</h1>
+    </div>
+            <div class="login-container__inputs">
+                <v-text-field label="User Name"></v-text-field>
+                <v-text-field label="Password"></v-text-field>
+                </div>
+                    <div class="login-container__btn">
+                        <v-btn>Login to the account</v-btn>
+                    </div> 
+    </div>
+
+</main>
+</template>
+
+<style scoped lang="scss">
+
+main{
+    margin: 8em;
+}
+.login-container{
+ width: 100%;
+ height: 100%;
+ display: flex;
+ flex-direction: column;
+
+&__img{
+    width: 10em;
+    
+}
+
+}
+
+
+</style>
--- src/main/frontend/src/views/HomeView.vue
+++ src/main/frontend/src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <script setup>
-
+import LoginComponent from '@/components/login/LoginComponent.vue';
 </script>
 
 <template>
-    
+    <LoginComponent/>
 </template>
 
