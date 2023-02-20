2b607a3532898d39de613ea4eeb4fd5ffe12cb97
David Daza <drdaza18@gmail.com>
2023-02-20 22:21:05 +0100

    configurando rutas y añadiendo default config

--- src/main/frontend/src/views/HomeView.vue
+++ src/main/frontend/src/views/HomeView.vue
@@ -3,6 +3,14 @@
 </script>
 
 <template>
+    <main>
     <h1>Somos el grupo de tercero b y venimos a hablar de la celula</h1>
+    </main>
 </template>
 
+<style lang="scss" scoped>
+    main{
+        width: 100%;
+        height: 100vh;
+    }
+</style>
