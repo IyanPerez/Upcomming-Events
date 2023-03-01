b823e0d2eb916d497281c0f3daf16b8dc3fb2476
blake S <design.harls@icloud.com>
2023-03-01 12:46:20 +0100

    added auth

---
+++ src/main/java/com/sala78/upcommingevents/controllers/AuthController.java
@@ -0,0 +1,29 @@
+package com.sala78.upcommingevents.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
+public class AuthController {
+
+    @GetMapping(value = "/login")
+    public ResponseEntity<Map<String,String>> access(){
+         var auth = SecurityContextHolder.getContext().getAuthentication();
+
+        Map<String, String> json = new HashMap<>();
+        json.put("message","Logged");
+        json.put("role", auth.getAuthorities().iterator().next().toString());
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(json);
+    }
+
+    
+}
