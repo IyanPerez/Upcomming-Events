635076849c27cab4480b0e8d334c7f354352291c
blake S <design.harls@icloud.com>
2023-03-08 12:31:32 +0100

    added store features events

--- src/main/frontend/src/stores/eventsStore.js
+++ src/main/frontend/src/stores/eventsStore.js
@@ -1,8 +1,17 @@
+import { defineStore } from "pinia";
+
 //generar un store que guarde los eventos destacados
-let featuresEvents = [];
+export const eventsStore = defineStore("featuresEvents", {
 
+  id: 'featuresEvents',
+  state: () => ({
+    fevents: [],
+    events: [],
+  }),
 
-
-
-
-
+  actions: {
+    savefEvents(title, imagen) {
+      events.push(this.fevents);
+    },
+  },
+});
