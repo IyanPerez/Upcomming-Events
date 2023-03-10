561812c21b3a1adb07a95252b4c5dc831ead843e
David Daza <drdaza18@gmail.com>
2023-03-10 18:03:27 +0100

    added deleteService

---
+++ src/main/java/com/sala78/upcommingevents/controllers/DeleteEventController.java
@@ -0,0 +1,15 @@
+package com.sala78.upcommingevents.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping(path = "/api/events/delete")
+public class DeleteEventController {
+    
+
+
+}
