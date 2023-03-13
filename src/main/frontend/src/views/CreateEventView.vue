b4093d99ba6ca352260d81eeb13406f48248b76c
Your Name <iyanperezmarcos@gmail.com>
2023-03-13 15:38:32 +0100

    card maquetacion

---
+++ src/main/frontend/src/views/CreateEventView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import CardsComponent from '../components/CardsComponent.vue';
+import DetailsForm from '../components/DetailsForm.vue';
+
+
+</script>
+
+<template>
+    <h1 class="mx-4 mt-4">Create Event</h1>
+    
+
+    <div class="main">
+        <CardsComponent/>
+        <DetailsForm/>
+    </div>
+
+
+</template>
+
+<style scoped lang="scss">
+
+.main{
+    display: flex;
+    flex-wrap: wrap;
+}
+
+</style>
