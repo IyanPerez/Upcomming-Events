96fab736600df27aa93fb3769c387f32eed4e182
blake S <design.harls@icloud.com>
2023-03-16 12:14:59 +0100

    added images

--- src/main/java/com/sala78/upcommingevents/storage/FileSystemStorageService.java
+++ src/main/java/com/sala78/upcommingevents/storage/FileSystemStorageService.java
@@ -10,20 +10,21 @@ import java.nio.file.StandardCopyOption;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.xml.sax.SAXException;
+
 
 @Service
 public class FileSystemStorageService  {
+    
     private final Path rootLocation;
-
+    
     @Autowired
     public FileSystemStorageService(StorageProperties properties){
         this.rootLocation = Paths.get(properties.getLocation());
     }
 
     
-    @Override
-    public void store(MultipartFile file){
+    
+    public void store(MultipartFile file) throws Exception{
         try {
             if(file.isEmpty()){
                 throw new Exception("Failed to store empty file.");
@@ -34,12 +35,12 @@ public class FileSystemStorageService  {
                 .normalize().toAbsolutePath();
             if (!destinationFile.getParent().equals(this.rootLocation.toAbsolutePath())){
                 //verifica que se esta guardando en el directorio correcto y no en cualquiera 
-                throw new Exception("Can´t store file outside current directory");
+                throw new Exception("Can't store file outside current directory");
 
             }
 // con esta linea le decimo consigue el bits (image) colocalo en el directorio y si hay una imagen con el mismo nombre me lo reemplzas
             try(InputStream inputStream = file.getInputStream()){
-                Files.copy(inputStream, destinationFile,StandardCopyOption.REPLACE_EXISTING))
+                Files.copy(inputStream, destinationFile,StandardCopyOption.REPLACE_EXISTING);
             }
         }
 
@@ -48,6 +49,11 @@ public class FileSystemStorageService  {
         }
     }
 
+   
+    
+    public Path load(String filename){
+        return rootLocation.resolve(filename);
+    }
     
 
 
--- src/main/java/com/sala78/upcommingevents/storage/StorageProperties.java
+++ src/main/java/com/sala78/upcommingevents/storage/StorageProperties.java
@@ -1,12 +1,19 @@
 package com.sala78.upcommingevents.storage;
 
+
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
 
 @ConfigurationProperties("storage")
+@Component// esta etiqueta es para que reconozca la configuracion
 public class StorageProperties {
 
     
     //location de las imagenes
+   
+
     private String location ="src/main/resources/images";
 
     public String getLocation(){
