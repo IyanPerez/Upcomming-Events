de147929351d87864521e9e2640a13b93b563ae7
blake S <design.harls@icloud.com>
2023-03-16 15:06:03 +0100

    show images

--- src/main/java/com/sala78/upcommingevents/config/SecurityConfig.java
+++ src/main/java/com/sala78/upcommingevents/config/SecurityConfig.java
@@ -40,10 +40,9 @@ public class SecurityConfig {
                                                 .logoutUrl("/api/logout")
                                                 .deleteCookies("JSESSIONID"))
                                 .authorizeRequests((auth) -> auth
-                                                .antMatchers("/api/register").permitAll()
+                                                .antMatchers("/api/register","/images/*").permitAll()
                                                 .antMatchers("/api/login").hasAnyRole("ADMIN", "USER")
                                                 .antMatchers("/api/events").hasAnyRole("ADMIN", "USER")
-                                                .antMatchers("/api/images/**").permitAll()
                                                 .antMatchers("/api/events/**").hasAnyRole("ADMIN", "USER")
                                                 .antMatchers("/api/user").hasRole("USER")
                                                 .antMatchers("/api/users/**").hasRole("USER")
