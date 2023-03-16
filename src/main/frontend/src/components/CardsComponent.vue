f76907b3c12cb3ffe20722d31f7506aeafe12647
Xavi <xavi9415@hotmail.com>
2023-03-16 14:44:08 +0100

    modificacion card, v-card desde vuetify

--- src/main/frontend/src/components/CardsComponent.vue
+++ src/main/frontend/src/components/CardsComponent.vue
@@ -2,50 +2,86 @@
 import { defineProps } from "vue";
 
 import addButton from '../components/AddButton.vue'
- const props = defineProps({
-    event:{
-        type: Object
-    }
- })
- const emits = defineEmits(['addEvent'])
+const props = defineProps({
+  event: {
+    type: Object
+  }
+})
+const emits = defineEmits(['addEvent'])
 
- const emitAddEvent = () => {
+const emitAddEvent = () => {
   emits('addEvent', props.event.id)
- }
+}
 
 
 </script>
-
 <template>
-        <div class="card">
-          <div class="card__image">
-            <v-btn id="edit__icon" variant="outlined" size="large" icon color="white">
-              <v-icon>mdi-pencil</v-icon>
-            </v-btn>
-          </div>
+  <v-card 
+    class="mx-auto "
+    max-width="100%"
+  >
+    <v-img
+      class="align-end text-white"
+      height="200"
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      cover
+    >
+      <v-card-title> <h3>{{ event.title }} </h3></v-card-title>
+    </v-img>
 
-          <div class="card__description">
-            <div id="icons">
-              <p class="card__date">{{event.date_hour}}</p>
-              <div id="capacity">
-                <img
-                  src="../assets/img/vector-people-cap.png"
-                  alt="people-vector"
-                  />
-                <p class="card__capacity">{{event.number_participants}}</p>
+    <v-card-subtitle class="pt-4 d-flex justify-space-between">
+      <p class="card__date">{{ event.date_hour }}</p>
+      <div id="capacity">
+        <v-icon>mdiAccount</v-icon>
+          <p class="card__capacity">Max.{{ event.number_participants }}</p>
 
-              </div>
-            </div>
-            <div id="description">
-              <h3>{{event.title}} </h3>
-
-              <p>{{ event.description }} </p>
-
-            </div>
-            <addButton @click="emitAddEvent()"/>
-          </div>
         </div>
+    </v-card-subtitle>
+
+    <v-card-text>
+      <h3>{{ event.title }} </h3>
+<p>{{ event.description }} </p>
+      
+    </v-card-text>
+
+    <v-card-actions>
+     
+
+      <v-btn color="orange">
+        <addButton @click="emitAddEvent()" />
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
+
+
+<!-- <template>
+  <div class="card">
+    <div class="card__image">
+      <v-btn id="edit__icon" variant="outlined" size="large" icon color="white">
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+    </div>
+
+    <div class="card__description">
+      <div id="icons">
+        <p class="card__date">{{ event.date_hour }}</p>
+        <div id="capacity">
+          <img src="../assets/img/vector-people-cap.png" alt="people-vector" />
+          <p class="card__capacity">{{ event.number_participants }}</p>
+
+        </div>
+      </div>
+      <div id="description">
+        <h3>{{ event.title }} </h3>
+
+        <p>{{ event.description }} </p>
+
+      </div>
+      <addButton @click="emitAddEvent()" />
+    </div>
+  </div>
+</template> -->
 
 <style scoped lang="scss">
 .card {
@@ -66,6 +102,7 @@ import addButton from '../components/AddButton.vue'
       margin: 4%;
     }
   }
+
   &__description {
     display: flex;
     gap: 5%;
@@ -75,6 +112,7 @@ import addButton from '../components/AddButton.vue'
       display: flex;
       flex-direction: column;
       justify-content: space-around;
+
       #capacity {
         display: flex;
         flex-direction: column;
@@ -91,5 +129,4 @@ import addButton from '../components/AddButton.vue'
     }
   }
 }
-
 </style>
---
+++ src/main/frontend/src/views/UserEventsView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import CardsComponent from '../components/CardsComponent.vue';
+import DetailsForm from '../components/DetailsForm.vue';
+import { computed } from 'vue';
+import { useLoginStore } from '../stores/loginStore';
+import axios from 'axios';
+import HeaderComponent from '../components/header/HeaderComponent.vue';
+import FeaturedEvents from '../components/FeaturedEvents.vue';
+
+// const useloginstore = useLoginStore();
+</script>
+<template>
+ <!-- <FeaturedEvents /> -->
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
